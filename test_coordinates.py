2ce3ff1868f33077c36dce9bc9a4cd60f042bdab
Артемій <44300059+Katerunner@users.noreply.github.com>
2019-05-11 10:07:38 +0300

    Add files via upload

---
+++ test_coordinates.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from coordinates import *
+import unittest
+
+
+class TestCoordinates(TestCase):
+    def setUp(self):
+        self.coord = Corray(50, 12)
+    def test_len(self):
+        self.assertEqual(len(self.coord), 2)
+    def test_lat(self):
+        self.assertEqual(self.coord['lat'], 50)
+    def test_lon(self):
+        self.assertEqual(self.coord['lon'], 12)
+    def test_set(self):
+        self.coord['lat'] = 66
+        self.coord['lon'] = 33
+        self.assertEqual(self.coord['lat'], 66)
+        self.assertEqual(self.coord['lon'], 33)
+
+
+unittest.main()
